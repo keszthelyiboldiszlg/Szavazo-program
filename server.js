72735936d001c4896fc177a2324edd04b814e18e
szt2-03 <szt2-03@szlgbp.hu>
2021-05-31 13:06:56 +0200

    css és logba írás

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ app.use(express.static('public'))
     response.end('wauwau');
 }); */
 
+app.use(express.urlencoded());
+app.post('/szavazas', function(request, response) {
+	console.log(request.body);
+    response.redirect("/");
+})
+
 app.listen(9000);
